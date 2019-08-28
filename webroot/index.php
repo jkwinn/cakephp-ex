665919f701babd1df245cfeb93d2d11a43aa72d7
jkwinn <jeffrey.winn@gmail.com>
2019-08-28 07:53:11 -0700

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,9 +1,13 @@
 <?php
+$load = sys_getloadavg();
+$proc = exec('nproc');
+$normload = $load/$proc;
 $x = 0.0001;
 for ($i = 0; $i <= 1000000; $i++) {
 	$x += sqrt($x);
 }
 echo "Pod Name: ";
 echo php_uname('n');
-echo "  - - - Hello All. This is a  test, simple webhook demo. Enjoy! "
+echo "  - - - Hello All. This is a  test, simple webhook demo. Enjoy! ";
+echo $normload;	
 ?>
