74658d98e912c4787c575461dfc025f32f57b1f6
jkwinn <jeffrey.winn@gmail.com>
2019-08-28 08:17:58 -0700

    add calc

--- webroot/index.php
+++ webroot/index.php
@@ -8,5 +8,7 @@ for ($i = 0; $i <= 1000000; $i++) {
 echo "Pod Name: ";
 echo php_uname('n');
 echo "  - - - Hello All. This is a  test, simple webhook demo. Enjoy! ";
-echo $proc
+echo $proc;
+<p><span class="description">Server Memory Usage:</span> <span class="result">= get_server_memory_usage() ?&gt;%</span></p>;
+<p><span class="description">Server CPU Usage: </span> <span class="result">= get_server_cpu_usage() ?&gt;%</span></p>
 ?>
