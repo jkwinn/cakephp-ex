e07d6728e8571dc7062360c8f24372c135837b86
jkwinn <jeffrey.winn@gmail.com>
2019-08-28 01:22:16 -0700

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,4 +1,7 @@
 <?php
+$load = sys_getloadavg();
+$proc = exec('nproc');
+$normload = $load/$proc;
 $x = 0.0001;
 for ($i = 0; $i <= 1000000; $i++) {
 	$x += sqrt($x);
@@ -6,5 +9,5 @@ for ($i = 0; $i <= 1000000; $i++) {
 echo "Pod Name: ";
 echo php_uname('n');
 echo "  - - - Hello All. This is a  test, simple webhook demo. Enjoy! "
-<p><span class="description">Server CPU Usage: </span> <span class="result">= get_server_cpu_usage() ?&gt;%</span></p>
+echo $load
 ?>
