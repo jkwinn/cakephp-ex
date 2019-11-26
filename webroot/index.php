56a2356ea785f800d0a12ed39e8080134b966782
jkwinn <jeffrey.winn@gmail.com>
2019-11-26 12:15:28 -0800

    not sure what happened

--- webroot/index.php
+++ webroot/index.php
@@ -27,7 +27,7 @@ echo "Pod Name: ";
 echo php_uname('n');
 echo "  - - - Hello All. This is a  test, simple webhook demo. Enjoy! ";
 echo $proc;
-echo <br><br>;
-<p><span class="description">Server Memory Usage:</span> <span class="result">= get_server_memory_usage() ?&gt;%</span></p>;
-<p><span class="description">Server CPU Usage: </span> <span class="result">= get_server_cpu_usage() ?&gt;%</span></p>
+#echo <br><br>;
+#<p><span class="description">Server Memory Usage:</span> <span class="result">= get_server_memory_usage() ?&gt;%</span></p>;
+#<p><span class="description">Server CPU Usage: </span> <span class="result">= get_server_cpu_usage() ?&gt;%</span></p>
 ?>
